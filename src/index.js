1d2b012c65196ade645c23977e40f17f51f0110a
Antoine Duval <antoine@cuffel.fr>
2022-08-13 11:25:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,15 +7,11 @@ class ChatBot extends LIBRARIES.Skill {
     super(_main, _settings, _folder);
     const SELF = this;
 
-    console.log(_settings);
-    console.log("loading ...");
     for(let i = 0; i < _settings.length; i++){
       const INTENT = "chatbot.custom_intent_" + i;
-      console.log(INTENT + " loaded.");
       _main.Manager.addDocuments(INTENT, _settings[i].utterances);
       _main.Manager.addAnswers(INTENT, _settings[i].answers);
     }
-    console.log("loaded");
   }
 }
 
